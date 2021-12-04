31ac18dd75b0ead27c9707e33fbd25d083087c5b
mert577 <kralmert577@gmail.com>
2021-12-04 15:53:14 +0300

    characteranim

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,12 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField]
     private float movementSpeed;
-
+    Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -22,6 +23,10 @@ public class PlayerMovement : MonoBehaviour
     {
         movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         movementDirection.Normalize();
+        anim.SetFloat("Horizontal", -movementDirection.x);
+        anim.SetFloat("Vertical", -movementDirection.y);
+        anim.SetFloat("Speed", movementDirection.magnitude);
+
     }
 
     private void FixedUpdate()
